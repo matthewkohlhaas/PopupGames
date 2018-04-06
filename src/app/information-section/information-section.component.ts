3823ed5d1b77da783fece9f6550317f90b9c50ec
Matthew Kohlhaas <matthewkohlhaas@gmail.com>
2018-04-05 22:31:09 -0400

    Update team member info

--- src/app/information-section/information-section.component.ts
+++ src/app/information-section/information-section.component.ts
@@ -10,27 +10,39 @@ export class InformationSectionComponent {
   readonly MEMBERS = [
     {
       name: 'Hung Do',
-      information: ['Information goes here', 'More info here'],
-      email: 'Email Goes here',
-      imageUrl: 'https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/12191147_987827851273686_3745170426158390146_o.jpg?_nc_cat=0&oh=30dec04f740'
-        + '756b8eb5256cbf6e838fa&oe=5B316BAF'
+      information: [
+        'Fifth-year student at Georgia Institute of Technology pursing a BS in Computer Science',
+        'Will graduate May 2018',
+        'Full stack developer at ADP'
+      ],
+      email: 'hdhungdo@gmail.com',
+      imageUrl: 'assets/hd.jpg'
     }, {
       name: 'Kavir Jones',
-      information: ['Information goes here', 'More info here'],
-      email: 'Email Goes here',
-      imageUrl: 'http://mhcsa.org.au/wp-content/uploads/2016/08/facebook-default-no-profile-pic.jpg'
+      information: [
+        'Fifth-year student at Georgia Institute of Technology pursing a BS in Computer Science',
+        'Will graduate December 2018',
+      ],
+      email: 'kjones319@gatech.edu',
+      imageUrl: 'assets/kj.jpg'
     }, {
       name: 'Matthew Kohlhaas',
-      information: ['Senior at Georgia Institute of Technology pursing a BS in Computer Science', 'Will graduate Spring 2018'],
+      information: [
+        'Fifth-year student at Georgia Institute of Technology pursing a BS in Computer Science',
+        'Will graduate May 2018',
+        'Full stack software engineer with Angular, Spring, and Node Express experience'
+      ],
       email: 'mkohlhaas3@gatech.edu',
-      imageUrl: 'https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/1079052_10201330734523096_774945990_o.jpg?_nc_cat=0&oh=71fff593b83c2d16b902'
-        + '9ebe821f2ffa&oe=5B6AC191'
+      imageUrl: 'assets/mk.jpg'
     }, {
       name: 'Matt McCoy',
-      information: ['Information goes here', 'More info here'],
-      email: 'Email Goes here',
-      imageUrl: 'https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/29749245_10214666654207469_3452371945575167144_o.jpg?_nc_cat=0&oh=1eece5005'
-        + 'a86d45a0d716fce5e225177&oe=5B64141F'
+      information: [
+        'Fifth-year student at Georgia Institute of Technology pursing a BS in Computer Science',
+        'Will graduate May 2018',
+        'Has a passion for Android development'
+      ],
+      email: 'mmccoy37@gatech.edu',
+      imageUrl: 'assets/mm.jpg'
     }
   ];
 
